a0b7b0fb9e8ad581a4f90b69644e0a78e0e17dbb
Nasta <anastasiiaminzhiner@mail.ru>
2024-02-22 10:59:16 +0300

    new file:   Laba 2/Division.cs

---
+++ Laba 2/Division.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Division
+{
+	static int Div(int A, int B)
+	{
+        if (B == 0) {
+            WriteLine("You can't divide by zero!");
+            return 0;
+        }
+        return A / B;
+    }
+}
